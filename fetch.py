2102882ea68880bb544087af994c8874e4017be3
Aiden Dowell <Adonamor@gmail.com>
2022-01-14 17:27:38 -0500

    added loop to find token ownership

--- fetch.py
+++ fetch.py
@@ -10,7 +10,9 @@ header = {
 
 # ---------Contract NFT Transfers-----------
 # search limit
-limit = "10"
+limit = "100"
+# offset
+offset = "0"
 # format of numbers
 type = "hex"
 # store
@@ -18,9 +20,12 @@ type = "hex"
 store = "0x495f947276749ce646f68ac8c248420045cb7b5e" #opensea
 # store = "0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb"# cryptopunks
 
-# GET
-response = requests.get("https://deep-index.moralis.io/api/v2/nft/" + store + "/transfers?chain=eth&format=" + type + "&limit=" + limit, headers = header)
-
+# get store transfers
+try:
+    response = requests.get("https://deep-index.moralis.io/api/v2/nft/" + store + "/transfers?chain=eth&offset=" + offset + "&format=" + type + "&limit=" + limit, headers = header)
+# get store trades
+except requests.exceptions.RequestException as e:
+    raise SystemExit(e)
 # grab json data
 j = response.json()
 # normilze the nested json -- grabbing the results column
@@ -30,14 +35,30 @@ df = pd.DataFrame.from_dict(norm)
 
 # grab from addresses
 from_addr = df["from_address"]
+# grab token id
+token_id = df["token_id"]
 
 # print data to file
 df.to_csv('./data/data.csv')
 # ------------------------------------------
 
+# ------Fetch Sender Data-------------------
+# for addr in from_addr:
+#     data = requests.get("https://deep-index.moralis.io/api/v2/" + addr + "/nft/transfers?chain=eth&format=hex&direction=both&limit=" + "3", headers = header).json()
+#     n = pd.json_normalize(data, "result")
+#     frame = pd.DataFrame.from_dict(n)
+#     frame.to_csv('./data/' + addr + ".csv")
+# -----------------------------------------
 
-for addr in from_addr:
-    data = requests.get("https://deep-index.moralis.io/api/v2/" + addr + "/nft/transfers?chain=eth&format=hex&direction=both&limit=" + "3", headers = header).json()
-    n = pd.json_normalize(data, "result")
-    frame = pd.DataFrame.from_dict(n)
-    frame.to_csv('./data/' + addr + ".csv")
+
+# ------Fetch Ownership Data---------------
+for token in token_id:
+    try:
+        ownership = requests.get("https://deep-index.moralis.io/api/v2/nft/" + store + "/" + token + "?chain=eth&format=" + type, headers = header)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+    ownership_json = ownership.json()
+    # n = pd.json_normalize(ownership, "result")
+    frame = pd.DataFrame.from_dict(ownership_json, orient='index')
+    frame.to_csv('./data/' + token + ".csv")
+# -----------------------------------------
